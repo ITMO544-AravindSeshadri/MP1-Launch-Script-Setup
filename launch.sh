4966cbb5ca7670dafda911e7e204017c269790fc
Aravind Seshadri <aravsesh1@gmail.com>
2015-11-07 23:43:07 -0600

    Including launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+./cleanup.sh
 declare -a instance_list
 mapfile -t instance_list < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file:///home/controller/Documents/MP1-Environment-Setup/install-env.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
 echo "Waiting for instance/instances ${instance_list[@]} to launch...."
